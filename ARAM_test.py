38648ff7a8dc1ab88ea3c9cc91822fd86bddd5ea
pntehan <2390003207@qq.com>
2019-03-17 20:18:37 +0800

    建模与数据预测

--- ARAM_test.py
+++ ARAM_test.py
@@ -2,7 +2,9 @@ from statsmodels.tsa.arima_model import ARIMA
 import pandas as pd
 import matplotlib.pyplot as plt
 
-trainSeting = pd.read_csv('./Data/yadz.csv').set_index('Time').fillna('bfill').head(365)
+trainSeting = pd.read_csv('./Data/yadz.csv').set_index('Time').head(365)
+ts_diff = trainSeting.diff(1).dropna()
+# print(ts_diff)
 model = ARIMA(trainSeting, order=(1, 1, 1))
 result_AR = model.fit(disp=-1)
 
@@ -17,14 +19,13 @@ forecast_AR = result_AR.forecast(forecast_n)[0]
 
 
 two = pd.read_csv('./Data/yadz.csv').set_index('Time')[365:730]
-AR_restored = pd.Series(two.Data, index=two.index).append(forecast_AR).cumsum()
+AR_restored = forecast_AR
 print(AR_restored)
-
-new = pd.DataFrame({'t': two.index, 'd': forecast_AR}).set_index('t')
+new = pd.DataFrame({'Time': two.index, 'Data': AR_restored}).set_index('Time')
 plt.plot(two, color='blue')
 plt.plot(new, color='red')
 plt.show()
-plt.savefig('first_model.png')
+# plt.savefig('first_model.png')
 
 
 
